3a38a5ba8410d56c1a6fdf0f499f964675d2d815
christopher <coronac511@gmail.com>
2021-04-20 14:53:41 -0400

    project started

---
+++ index.js
@@ -0,0 +1,24 @@
+let value = "New Jersey"
+
+function takedata(value) {
+   
+fetch(`https://corona.lmao.ninja/v2/states/${value}?yesterday=`, {
+    headers: {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    }
+})
+
+.then(function (response) {
+    return response.json();
+})
+
+.then(function(data) {
+    console.log(data)
+})
+}
+
+ 
+const searchbox = document.getElementById ('search')
+const  submitbtn = document.getElementById ('submit-button')
+submitbtn.addEventListener ('click', takedata(searchbox.value))
+
