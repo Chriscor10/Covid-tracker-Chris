2cc06f1b2178c3b33b117522eb794fcc919977da
christopher <coronac511@gmail.com>
2021-04-21 13:31:07 -0400

    Appendign data to the DOM

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ fetch(`https://corona.lmao.ninja/v2/states/${value}?yesterday=`, {
 })
 
 .then(function(data) {
-    console.log(data)
+const nHeader1 = document.createElement('h1')    
+nHeader1.innerHTML = data.state
+document.body.append(nHeader1)
+
+const nHeader3 = document.createElement('h3')
+nHeader3.innerHTML = 'Total of Cases'
+document.body.append(nHeader3)
+
+const info1 = document.createElement('h4')
+info1.innerHTML = (data.cases)
+document.body.append(info1)
 })
-}
+}   
 
  
 const searchbox = document.getElementById ('search')
