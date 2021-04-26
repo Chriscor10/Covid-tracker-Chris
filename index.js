a369eaf9478a714b44bb1f86a4c7c827bacafaf3
christopher <coronac511@gmail.com>
2021-04-26 19:53:28 -0400

    project review

--- index.js
+++ index.js
@@ -13,50 +13,58 @@ fetch(`https://corona.lmao.ninja/v2/states/${value}?yesterday=`, {
 
 .then(function(data) {
 
+   
+    const newState = document.createElement('div')
+     const statecontainer = document.getElementById("statecontainer")
+     statecontainer.innerHTML = ""
+     statecontainer.append(newState)
+   
+
+
     const nHeader1 = document.createElement('h1')    
 nHeader1.innerHTML = data.state
-document.body.append(nHeader1)
+newState.append(nHeader1)
 
 const nHeader2 = document.createElement('h3')
 nHeader2.innerHTML = 'Total of Cases'
-document.body.append(nHeader2)
+newState.append(nHeader2)
 
 const info1 = document.createElement('h4')
 info1.innerHTML = (data.cases)
-document.body.append(info1)
+newState.append(info1)
 
 
 const nHeader3 = document.createElement('h3')
 nHeader3.innerHTML = "Today's Cases"
-document.body.append(nHeader3)
+newState.append(nHeader3)
 
 const info2 = document.createElement('h4')
 info2.innerHTML = (data.todayCases)
-document.body.append(info2)
+newState.append(info2)
 
 const nHeader4 = document.createElement('h3')
 nHeader4.innerHTML = "Deaths"
-document.body.append(nHeader4)
+newState.append(nHeader4)
 
 const info3 = document.createElement('h4')
 info3.innerHTML = (data.deaths)
-document.body.append(info3)
+newState.append(info3)
 
 const nHeader5 = document.createElement('h3')
 nHeader5.innerHTML = ("Today's Deaths")
-document.body.append(nHeader5)
+newState.append(nHeader5)
 
 const info4 = document.createElement('h4')
 info4.innerHTML = (data.todayDeaths)
-document.body.append(info4)
+newState.append(info4)
 
 const nHeader6 = document.createElement('h3')
 nHeader6.innerHTML = ("Active Cases")
-document.body.append(nHeader6)
+newState.append(nHeader6)
 
 const info5 = document.createElement('h4')
 info5.innerHTML = (data.active)
-document.body.append(info5)
+newState.append(info5)
 
 
 })
